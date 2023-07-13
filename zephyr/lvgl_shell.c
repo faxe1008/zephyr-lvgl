e1151688010d736646f6a23e25b3fc919149b1db
faxe1008 <fabianblatz@gmail.com>
2023-07-13 10:34:11 +0200

    Stuff

--- zephyr/lvgl_shell.c
+++ zephyr/lvgl_shell.c
@@ -5,36 +5,90 @@
  */
 
 #include <zephyr/shell/shell.h>
+#include <zephyr/logging/log.h>
 #include <lvgl.h>
 #include <autoconf.h>
+
+LOG_MODULE_REGISTER(lvgl_shell, CONFIG_LV_LOG_LEVEL);
 
 #ifdef CONFIG_LV_Z_MEM_POOL_SYS_HEAP
 #include "lvgl_mem.h"
 #endif
 
+#ifdef CONFIG_LV_USE_PERF_MONITOR
 
-static int cmd_lvgl_mem_stat(const struct shell *sh, size_t argc,
+void perf_sample(struct k_timer *timer){
+    ARG_UNUSED(timer);
+
+    uint32_t avg_fps = lv_refr_get_fps_avg();
+
+    lv_refr_reset_fps_counter();
+}
+
+K_TIMER_DEFINE(perf_sample_timer, perf_sample, NULL);
+#endif
+
+static int cmd_lvgl_stats(const struct shell *sh, size_t argc,
 			    char *argv[])
 {
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+
+    shell_help(sh);
+    return SHELL_CMD_HELP_PRINTED;
+}
+
+static int cmd_lvgl_stats_memory(const struct shell *sh, size_t argc,
+			    char *argv[])
+{
+#ifdef CONFIG_LV_Z_MEM_POOL_SYS_HEAP
     bool dump_chunks = false;
 
-    if(argc == 2){
-        if(strcmp(argv[1], "--chunks")){
+    if(argc == 2) {
+        if(strcmp(argv[1], "-c") == 0){
             dump_chunks = true;
+        } else {
+            shell_error(sh, "unsupported option %s", argv[1]);
+            shell_help(sh);
+            return SHELL_CMD_HELP_PRINTED;
         }
     }
 
     lvgl_print_heap_info(dump_chunks);
-
+#else
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+    shell_error(sh, "Set CONFIG_LV_Z_MEM_POOL_SYS_HEAP to enable memory statistics support.");
+#endif
     return 0;
 }
 
+static int cmd_lvgl_stats_perf(const struct shell *sh, size_t argc,
+			    char *argv[])
+{
+#ifdef CONFIG_LV_USE_PERF_MONITOR
+    return 0;
+#else
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+    shell_error(sh, "Set CONFIG_LV_USE_PERF_MONITOR to enable performance statistics support.");
+#endif
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(lvgl_cmd_stats,
+	SHELL_CMD_ARG(memory, NULL, 
+        "Show LVGL memory statistics\n"
+        "Usage: lvgl stats memory [-c]\n"
+		"-c  dump chunk information\n", cmd_lvgl_stats_memory, 1, 1),
+    SHELL_CMD_ARG(perf, NULL,
+        "Start or stop performance profiling\n"
+        "Usage: lvgl stats perf <start|stop>", cmd_lvgl_stats_perf, 2, 0),
+	SHELL_SUBCMD_SET_END
+);
 
 
 SHELL_STATIC_SUBCMD_SET_CREATE(lvgl_cmds,
-#ifdef CONFIG_LV_Z_MEM_POOL_SYS_HEAP
-	SHELL_CMD_ARG(mem_stat, NULL, "Print LVGL memory statistics", cmd_lvgl_mem_stat, 1, 1),
-#endif
+	SHELL_CMD(stats, &lvgl_cmd_stats, "Show LVGL statistics", cmd_lvgl_stats),
 	SHELL_SUBCMD_SET_END
 );
 
